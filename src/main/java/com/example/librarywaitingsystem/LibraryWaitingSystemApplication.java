880f321fa6589cb91ff887ae94e74d765bfbd2eb
1117mg <lucia_17@naver.com>
2024-11-27 12:46:18 +0900

    Initial commit

---
+++ src/main/java/com/example/librarywaitingsystem/LibraryWaitingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarywaitingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWaitingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryWaitingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/librarywaitingsystem/LibraryWaitingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarywaitingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWaitingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
